ad1a4d03c08eca3accb1c1ce5a3f51af1153329d
Alexey <monticorik@gmail.com>
2023-08-25 21:12:53 +0300

    feat: added functional for voting page

--- src/Components/pages/votingPage/VotingPage.js
+++ src/Components/pages/votingPage/VotingPage.js
@@ -1,11 +1,91 @@
+import { useState, useEffect } from "react";
+import useCatServices from "../../../services/CatServices";
+
 import AppWrapper from "../../appWrapper/AppWrapper";
 import Label from "../../label/Label";
 import { BackButton, UpdateButton } from "../../buttons/Buttons";
+import Spinner from "../../spinner/Spinner";
 
 import "./votingPage.scss";
-import cat from "../../../resources/img/news_file_3846_5ea8a69e48534.jpg";
 
 const VotingPage = () => {
+    const {getAllImages, getVotings, getFavouritings, setVote, setFavourite} = useCatServices();
+    const [image, setImage] = useState({});
+    const [votes, setVotes] = useState([]);
+    const [favourites, setFavourites] = useState([]);
+    const [voteLog, setVoteLog] = useState([]);
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [imageLoading, setImageLoading] = useState(true);
+    const [logLoading, setLogLoading] = useState(true);
+
+    const page = 0;
+    const limit = 1;
+
+    const addToLike = () => {
+        setBtnDisabled(true);
+        setLogLoading(true);
+        setVote({vote: 1, imageId: image.id})
+        .then(onLoadVotes);
+    };
+
+    const addToFavourite = () => {
+        setBtnDisabled(true);
+        setLogLoading(true);
+        setFavourite({imageId: image.id})
+        .then(onLoadVotes);
+    };
+
+    const addToDislike = () => {
+        setBtnDisabled(true);
+        setLogLoading(true);
+        setVote({vote: 0, imageId: image.id})
+        .then(onLoadVotes);
+    };
+
+    const onLoadVotes = () => {
+        getVotings({})
+        .then(res => setVotes(res));
+
+        getFavouritings({})
+        .then(res => setFavourites(res));
+    };
+
+    useEffect(() => {
+        getAllImages({page, limit})
+        .then(res => {
+            setImage(...res);
+            setImageLoading(false);
+        });
+
+        onLoadVotes();
+    }, []);
+
+    useEffect(() => {
+        setVoteLog(() => 
+            votes.concat(favourites)
+                 .slice()
+                 .sort((a, b) => a.createdTime < b.createdTime ? 1 : -1)
+                 .map(vote => {
+                 return(
+                    <div className="log_row"
+                        key={vote.id}>
+                        <span className="time">{vote.createdTime}</span>
+                            <span className="log_text">Image ID: <strong>{vote.imageId}</strong> was added to {vote.value}s</span>
+                        <i className={`icon_${vote.value}`}></i>
+                    </div>
+                 );
+        }));
+
+        setLogLoading(false);
+
+        setBtnDisabled(false);
+    }, [votes, favourites]);
+
+    const imageSpiner = imageLoading ? <Spinner/> : null;
+    const logSpiner = logLoading ? <Spinner/> : null;
+    const loadedImage = !imageLoading && image ? <img src={image.src} alt="cat" /> : null;
+    const log =  !logLoading && voteLog ? voteLog : null;
+    
     return(
         <AppWrapper>
             <aside className="filters_section voting_page">
@@ -15,26 +95,30 @@ const VotingPage = () => {
             </aside>
             <section className="img_block">
                 <div className="image">
-                    <img src={cat} alt="cat" />
+                    {imageSpiner}
+                    {loadedImage}
                 </div>
                 <div className="voting_button_block">
-                    <button className="add_to_like">
+                    <button className="add_to_like"
+                            onClick={addToLike}
+                            disabled={btnDisabled}>
                         <i className="icon_like"></i>
                     </button>
-                    <button className="add_to_favourite">
+                    <button className="add_to_favourite"
+                            onClick={addToFavourite}
+                            disabled={btnDisabled}>
                         <i className="icon_favourite"></i>
                     </button>
-                    <button className="add_to_dislike">
+                    <button className="add_to_dislike"
+                            onClick={addToDislike}
+                            disabled={btnDisabled}>
                         <i className="icon_dislike"></i>
                     </button>
                 </div>
             </section>
             <section className="log_block">
-                <div className="log_row">
-                    <span className="time">22:06</span>
-                    <span className="log_text">Image ID: <strong>3ll</strong> was added to Likes</span>
-                    <i className="icon_like"></i>
-                </div>
+                {logSpiner}
+                {log}
             </section>
         </AppWrapper>
     );
--- src/services/CatServices.js
+++ src/services/CatServices.js
@@ -4,19 +4,49 @@ const useCatServices = () => {
     const {loading, error, request, clearError} = useHttp();
     const _apiBase = 'https://api.thecatapi.com/v1/';
     const _apiKey = 'api_key=e807a068-7ac5-4fc5-9c73-9e2c858d1264';
+    const _subId = 'The-greatest-user-ever';
 
     const getAllBreeds = async () => {
         const res = await request(`${_apiBase}breeds`);
         return res.map(_transformBreeds);
     };
 
-    const getAllImages = async ({limit = 50, page = 0, order = 'ASC', type ='jpg,png', breedId = ''}) => {
+    const getAllImages = async ({limit = 1, page = 0, order = 'RANDOM', type ='jpg,png', breedId = ''}) => {
         if(breedId){
             order = 'ASC';
         }
 
         const res = await request(`${_apiBase}images/search?limit=${limit}&page=${page}&order=${order}&mime_types=${type}&breed_ids=${breedId}&${_apiKey}`);
         return res.map(_transformImages);
+    };
+
+    const getVotings = async ({page = 0, limit = 100, order = 'DESC'}) => {
+        const res = await request(`${_apiBase}votes?page=${page}&limit=${limit}&order=${order}&${_apiKey}`);
+
+        return res.map(_transformVoting);
+    };
+
+    const getFavouritings = async ({page = 0, limit = 100, order = 'DESC'}) => {
+        const res = await request(`${_apiBase}favourites?page=${page}&limit=${limit}&order=${order}&${_apiKey}`);
+
+        return res.map(_transformVoting);
+    };
+
+    const setVote = async ({vote = 1, imageId}) => {
+        const body = {"image_id": imageId,
+                      "sub_id": _subId,
+                      "value": vote};
+        const res = await request(`${_apiBase}votes?${_apiKey}`, 'POST', undefined, JSON.stringify(body));
+
+        return res;
+    };
+
+    const setFavourite = async ({imageId}) => {
+        const body = {"image_id": imageId,
+                      "sub_id": _subId};
+        const res = await request(`${_apiBase}favourites?${_apiKey}`, 'POST', undefined, JSON.stringify(body));
+
+        return res;
     };
 
     const _transformBreeds = (breed) => {
@@ -39,7 +69,17 @@ const useCatServices = () => {
         };
     };
 
-    return {loading, getAllBreeds, getAllImages};
+    const _transformVoting = (vote) => {
+        return {
+            id: vote.id,
+            createdTime: vote.created_at.replace(/[T]|(:\d+\.\d+\w+)/gi, ' '),
+            value: vote.value === 1 ? 'like' : vote.value === 0 ? 'dislike' : 'favourite',
+            imageId: vote.image.id,
+            imageSrc: vote.image.url,
+        };
+    };
+
+    return {loading, getAllBreeds, getAllImages, getVotings, getFavouritings, setVote, setFavourite};
 };
 
 export default useCatServices;
